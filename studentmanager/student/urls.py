9b8c6ce15abb2dfcc08669b1f54765e514d9680b
jincheng hu <jinchenghu@jinchengdeMac-mini.local>
2021-02-28 17:43:30 +0800

    在家学习django2

--- studentmanager/student/urls.py
+++ studentmanager/student/urls.py
@@ -19,9 +19,29 @@ urlpatterns = [
 
     url(r'^get_path/$', views.get_path, name="获取路径"),
 
-    url(r'^classes/(?P<value2>\d+)/(?P<value1>.+)/$', views.classes, name="班级信息"),
+    url(r'^classes/(?P<value1>\d+)/(?P<value2>.+)/$', views.classes, name="班级信息"),
 
-    url(r'^canshu/(?P<value2>\d+)/(?P<value1>.+)/$', views.canshu, name="参数"),
+    url(r'^canshu/(?P<value1>\d+)/(?P<value2>.+)/$', views.canshu, name="参数"),
+
+    url(r'^studentone/$', views.studentone, name="stu1"),
+
+    url(r'^student2/$', views.student2, name="stu2"),
+
+    url(r'^student3/$', views.student3, name="stu3"),
+
+    url(r'^student4/$', views.student4, name="stu4"),
+
+    url(r'^student5/$', views.student5, name="stu5"),
+
+    url(r'^student6/$', views.student6, name="stu6"),
+
+    url(r'^set_cookie/$', views.set_cookie, name="set_cookie"),
+
+    url(r'^get_cookie/$', views.get_cookie, name="get_cookie"),
+
+    url(r'^set_session/$', views.set_session, name="set_session"),
+
+    url(r'^get_session/$', views.get_session, name="get_session"),
 
 ]
 
--- studentmanager/student/views.py
+++ studentmanager/student/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.shortcuts import redirect
+
 
 from django.urls import reverse
 
-
+import json
 # Create your views here.
 
 
@@ -13,6 +16,7 @@ def studentAll(request):
 
 
 def subject(request, value1, value2):
+    # 位置参数
 
     context = {"v1":value1, "v2":value2}
 
@@ -22,11 +26,96 @@ def subject(request, value1, value2):
 
 
 def classes(request,value2,value1):
+
+    # 关键字参数
+
     context = {"v1":value1, "v2":value2}
 
-
-
     return render(request, "student/classes.html", context)
+
+
+
+def studentone(request):
+    #查询字符串 query_string,注意QueryDict
+    data = request.GET
+    print(data)
+
+    name = data["name"]
+    banji = data.get("classes")
+
+    name2 = data.getlist("name")
+
+    print(name, banji, name2)
+
+    return HttpResponse("ok")
+
+def student2(request):
+
+    # 获取请求体里面的数据，post请求，form_data格式
+    data = request.POST
+    print(data)
+
+    name = data['name']
+    banji = data.get('banji')
+
+    name2 = data.getlist('name')
+    print(name,banji)
+    print(name2)
+
+    return HttpResponse("ok2")
+
+def student3(request):
+    # post请求，获取json格式的参数
+
+    data = request.body
+    data2 = data.decode()
+    print(data2)
+
+    print(type(data2))
+
+    res = json.loads(data2)
+    print(type(res))
+
+    name = res["name"]
+    banji = res.get("banji")
+
+    print(name, banji)
+
+    return HttpResponse("ok3")
+
+def student4(request):
+    # 获取请求头里面的数据和其他
+
+    header = request.META
+    # print(header)
+    # print(header['HTTP_TYPE'])
+
+    print(request.method)
+    print(request.user)
+    print(request.path)
+
+    return HttpResponse("ok4")
+
+
+def student5(resquest):
+    # HttpResponse JsonResponse响应
+
+    data = {"name":"关羽"}
+
+    # data = "guanyu"
+
+    return JsonResponse(data)
+
+    # return HttpResponse(content=data, status=300, content_type="text/plain")
+
+def student6(request):
+
+    # return redirect("http://www.baidu.com")
+
+    path = reverse("student:stu4")
+
+    return redirect(path)
+
 
 def canshu(request, value1, value2):
 
@@ -39,14 +128,54 @@ def canshu(request, value1, value2):
     return render(request, "student/canshu.html", context)
 
 
-
-
 def get_path(request):
 
-    url = reverse("student:subject")
-    url2 = reverse("student:获取路径")
+    url = reverse("student:stu1")
+    url2 = reverse("student:stu2")
 
     print(url)
     print(url2)
 
-    return HttpResponse("reverse 函数，返回路径")
+    return HttpResponse(url2)
+
+def set_cookie(request):
+
+    # 1:假设没有cookie
+
+    # 2:获取数据，设置cookie
+    name = request.GET.get("name")
+
+    response = HttpResponse("set_cookie")
+    response.set_cookie("name", name, max_age=3600)
+    # 3:返回cookie信息
+
+    return response
+
+def get_cookie(request):
+
+    data = request.COOKIES
+    name = data.get("name")
+    print(name)
+
+    return HttpResponse("get_cookie")
+
+def set_session(request):
+
+    print(request.COOKIES)
+
+    member_id = 6
+
+    request.session["member_id"] = member_id
+
+    return HttpResponse("set_session")
+
+
+def get_session(request):
+
+    print(request.COOKIES)
+
+    member_id = request.session["member_id"]
+
+    print(member_id)
+
+    return HttpResponse("get_session")
